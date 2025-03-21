47300b3c351d4142efc296b4e1ae5db04d2d4c37
Alexandre <lassaignealexandre@gmail.com>
2025-03-21 14:21:55 +0100

    portfolio

--- components/ThemeProvider.js
+++
@@ -1,8 +0,0 @@
-export default function ThemeProvider(props) {
-    const [theme, setTheme] = React.useState("light");
-    return (
-      <ThemeContext.Provider value={{ theme, setTheme }}>
-        {props.children}
-      </ThemeContext.Provider>
-    );
-  };
